0b9fa4a72664d9fdcec211e6d410613fa882dc06
nauar <nauar.rekmani@gmail.com>
2022-09-21 17:13:29 -0400

    first commit :)

---
+++ helloteam.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main(){
+    cout<<"Hello team, I hope we can get a good grade on this project" << ":)" <<endl;
+    for (int i = 0; i < 3; i++)
+    {
+        cout << "........." <<endl;
+    }
+    cout<< "Or else Emma will be pissed!"<<":("<< endl;
+    return 0;
+}
